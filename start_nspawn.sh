d6d6f5e3cb7462d6fa70852e6aedc7c7d9c93fbf
Bunnyton <galaktionovaa@student.bmstu.ru>
2024-05-10 00:50:10 +0300

    update start_nspawn.sh

--- start_nspawn.sh
+++ start_nspawn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo systemd-nspawn -q --boot -D ./lib/images/debootstrap/debian-stable --user student --overlay="$PWD/lib/images/debootstrap/debian-stable:$PWD/lib/images/base/student-debian:/" 
+sudo systemd-nspawn -b -q -D ./lib/images/debootstrap/debian-stable --user student --overlay="$PWD/lib/images/debootstrap/debian-stable:$PWD/lib/images/base/student-debian:/" script -q -c bash /var/log/hashpass.log
+
