c17420c1a5de9378c4af2ea0641ddf359d5e28f9
Bunnyton <galaktionovaa@student.bmstu.ru>
2024-05-10 04:18:18 +0300

    add script option for every step logging

--- start_nspawn.sh
+++ start_nspawn.sh
@@ -5,7 +5,7 @@
 spawn systemd-nspawn -b -q \
 	-D ./test \
 	--user root # \
-	# sudo script -q -c 'sudo -u student bash' /var/log/hashpass.log
+	sudo script -fqc 'sudo -u student bash' /var/log/hashpass.log
 	# -D ./lib/images/debootstrap/debian-stable \
 	# --overlay="$PWD/lib/images/debootstrap/debian-stable:$PWD/lib/images/base/student-debian:/" \
 
