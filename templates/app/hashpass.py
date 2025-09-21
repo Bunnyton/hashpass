6a6c24f29897724bcd5573d124baa1d3a6655ca2
debi <debi@debian.debi>
2025-09-22 02:27:46 +0300

    hashpass.py: start task with name task_num:latest

--- templates/app/hashpass.py
+++ templates/app/hashpass.py
@@ -85,7 +85,7 @@ def main():
         if task_number == 0 or tasks.get(str(task_number)) and \
             key(masterkey + config['username'] + str(task_number)) == tasks[str(task_number)]:
                 # Запуск задания
-            subprocess.run(['/'.join([HASHPASS_DIR, "make_env.py"]), "start", str(task_number) +":1.0"])
+            subprocess.run(['/'.join([HASHPASS_DIR, "make_env.py"]), "start", str(task_number) +":latest"])
 
         else:
             while True:
