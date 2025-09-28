78544a43fde7b84ff37ba4a703516706752316a2
debi <debi@debian.debi>
2025-09-28 20:21:20 +0300

    upgrade

--- pyarmor_runtime_0000002/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-09-28T18:14:35.836291
-from .pyarmor_runtime import __pyarmor__
--- templates/app/pyarmor_runtime_0000001/__init__.py
+++ templates/app/pyarmor_runtime_0000001/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-09-28T18:16:44.297645
+# Pyarmor 9.1.9 (trial), 000000, 2025-09-28T20:19:25.469885
 from .pyarmor_runtime import __pyarmor__
