a059d125cf0321154a04b1b7972e0370ecfec374
Your Name <you@example.com>
2025-09-29 18:34:44 +0000

    update

---
+++ install/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt -y install python3.13 python3.13-venv wget curl
+
+wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass.deb?ref_type=heads
+sudo apt install -f hashpass.deb
