8ee721065f8b9ac1f57135b6baa0973054693a76
Bunnyton <galaktionovaa@student.bmstu.ru>
2025-09-29 19:00:14 +0000

    Edit install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt -y install python3.13 python3.13-venv wget curl
 
-wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass.deb?ref_type=heads
+wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass.deb
 sudo apt install -f ./hashpass.deb
 rm ./hashpass.deb
 sudo hashpass
