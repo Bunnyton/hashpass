dcdbb67c416a9bc7422c7fb21cff0286e2b2b5d0
Your Name <you@example.com>
2025-09-29 18:01:32 +0000

    update

--- install/build.sh
+++ install/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install -y python3.13
-sudo apt install -y python3.13-venv
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.13 1
-sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/local/bin/pip3.13 1
+sudo dpkg-deb --build hashpass
+git add .
+git commit -m "update"
+git push -u origin main
