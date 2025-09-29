75ca954b36b2c4d251f51e9fd97a74b753bc333f
Bunnyton <galaktionovaa@student.bmstu.ru>
2025-09-29 19:03:19 +0000

    Edit install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,4 +7,3 @@ sudo apt -y install python3.13 python3.13-venv wget curl
 wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass.deb
 sudo apt install -y -f ./hashpass.deb
 rm ./hashpass.deb
-sudo hashpass
