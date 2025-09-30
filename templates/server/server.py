fee3a6281e9ae88b7e41fcc73d83d66b326376f9
Kirill <gluhov@bmstu.ru>
2025-09-30 19:34:42 +0300

    beautify server.py

--- templates/server/server.py
+++ templates/server/server.py
@@ -1,21 +1,20 @@
-from flask import Flask, request, send_file, jsonify, abort
-import os
-import toml
 import glob
 import json
+import os
 
+import toml
+from flask import Flask, abort, jsonify, request, send_file
 
 app = Flask(__name__)
-STORAGE_DIR = 'storage'
+STORAGE_DIR = "storage"
 os.makedirs(STORAGE_DIR, exist_ok=True)
 
 
 @app.route("/images", methods=["GET"])
 def list_images():
     """Список всех образов"""
-
     images = list()
-    for file in glob.glob(STORAGE_DIR + "/**/" + 'manifest.toml', recursive=False):
+    for file in glob.glob(STORAGE_DIR + "/**/" + "manifest.toml", recursive=False):
         temp = toml.load(file)
         images.append(temp)
 
@@ -24,25 +23,25 @@ def list_images():
 
 def get(param: str):
     temp = None
-    if '/' in param:
-        author, name = param.split('/')
-        if ':' in name:
-            name, version = name.split(':')
+    if "/" in param:
+        author, name = param.split("/")
+        if ":" in name:
+            name, version = name.split(":")
 
         else:
             version = "latest"
-         
-        for file in glob.glob(STORAGE_DIR + "/**/" + 'manifest.toml', recursive=False):
+
+        for file in glob.glob(STORAGE_DIR + "/**/" + "manifest.toml", recursive=False):
             temp = toml.load(file)
 
-            if temp['image']['name'] == name and temp['image']['version'] == version and temp['image']['author'] == author:
+            if temp["image"]["name"] == name and temp["image"]["version"] == version and temp["image"]["author"] == author:
                 return temp
 
     else:
-        for file in glob.glob(STORAGE_DIR + "/**/" + 'manifest.toml', recursive=False):
+        for file in glob.glob(STORAGE_DIR + "/**/" + "manifest.toml", recursive=False):
             temp = toml.load(file)
 
-            if temp['image']['id'] == param:
+            if temp["image"]["id"] == param:
                 return temp
 
     return None
@@ -55,14 +54,14 @@ def push_image():
 
     if manifest_raw and image_file:
         manifest = json.loads(manifest_raw)
-        if manifest['image']['id'] and manifest['image']['name'] and manifest['image']['version'] and 'layers' in manifest['image'] and manifest['image']['author'] and manifest['image']['type']:
-            image_path = os.path.join(STORAGE_DIR, manifest['image']['id'])
+        if manifest["image"]["id"] and manifest["image"]["name"] and manifest["image"]["version"] and "layers" in manifest["image"] and manifest["image"]["author"] and manifest["image"]["type"]:
+            image_path = os.path.join(STORAGE_DIR, manifest["image"]["id"])
 
             os.makedirs(image_path, exist_ok=True)
-            with open(os.path.join(image_path, 'manifest.toml'), 'w') as f:
+            with open(os.path.join(image_path, "manifest.toml"), "w") as f:
                 toml.dump(manifest, f)
-        
-            image_file.save(os.path.join(image_path, manifest['image']['id'] + '.tar.gz'))
+
+            image_file.save(os.path.join(image_path, manifest["image"]["id"] + ".tar.gz"))
             return f"Image {manifest['image']['author']}/{manifest['image']['name']}:{manifest['image']['version']} uploaded successfully", 200
 
 
@@ -83,7 +82,7 @@ def get_info():
 @app.route("/download/<image_id>", methods=["GET"])
 def download_image(image_id):
     """Отдаёт сам образ (без манифеста)"""
-    image_path = os.path.join(STORAGE_DIR, image_id, image_id + '.tar.gz')
+    image_path = os.path.join(STORAGE_DIR, image_id, image_id + ".tar.gz")
 
     if not os.path.exists(image_path):
         return abort(404, description="Image not found")
