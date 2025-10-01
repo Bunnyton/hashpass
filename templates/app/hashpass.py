953833e6fc0386e90758af4d1dd4b6683d29eebd
Bunnyton <toni.suvorov.15@gmail.com>
2025-10-01 18:22:02 +0300

    hashpass.py: fix error

--- templates/app/hashpass.py
+++ templates/app/hashpass.py
@@ -48,7 +48,7 @@ def main():
     print("\nОбновление списка заданий")
     try:
         for task_name in userconfig.get_task_name(all=True):
-            pull_cmd = ['/'.join([settings.sys_app_path]), "pull", task_name]
+            pull_cmd = ['python3.13', '/'.join([settings.sys_app_path]), "pull", task_name]
             exec_cmd(pull_cmd)
 
         print("Обновление завершено")
@@ -109,7 +109,7 @@ def main():
         if task_num == 0 or check_key(userconfig, task_num=task_num - 1):
             # Запуск задания
             try:
-                cmd = ['/'.join([settings.sys_app_path]), "start", userconfig.get_task_name(task_num)]
+                cmd = ['python3.13', '/'.join([settings.sys_app_path]), "start", userconfig.get_task_name(task_num)]
                 exec_cmd(cmd)
 
             except Exception as e:
