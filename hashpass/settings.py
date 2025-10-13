23a3bcdfa2213223683f131790d489399fb1462d
debi <debi@debian.debi>
2025-10-14 02:40:08 +0300

    add new tasks: end of base commands modules

--- hashpass/settings.py
+++ hashpass/settings.py
@@ -67,6 +67,11 @@ class Settings(BaseSettings):
             "16": "bunnyton/find:4_new",
             "17": "bunnyton/find:5",
             "18": "bunnyton/sudo:intro",
+            "19": "bunnyton/apt:install",
+            "20": "bunnyton/apt:remove",
+            "21": "bunnyton/apt:add_repo",
+            "22": "bunnyton/apt:deb",
+            "23": "bunnyton/sw:latest",
         },
         validation_alias = None
     )
