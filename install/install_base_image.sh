beaf0b9085ce7f063d89562b05696a8c4cb77647
Bunnyton <galaktionovaa@student.bmstu.ru>
2025-10-24 18:36:14 +0000

    Edit install_base_image.sh

--- install/install_base_image.sh
+++ install/install_base_image.sh
@@ -4,10 +4,7 @@ apt update
 apt upgrade -y
 
 apt install -y rsync python3 fish sudo
-apt install -y python3-watchdog \ 
-                    python3-numpy \
-                    python3-toml \
-                    python3-tqdm
+apt install -y python3-watchdog python3-numpy python3-toml python3-tqdm
 apt install -y net-tools curl wget
 
 sed -i "s/\/usr\/bin\/bash/\/usr\/bin\/hash/g" /etc/passwd
