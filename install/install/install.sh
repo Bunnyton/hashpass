b06e9792c46b36cb908521a156725150c929a33d
debi <debi@debian.debi>
2025-12-08 03:02:52 +0300

    add tasks 30

--- install/install/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt -y install python3.13 python3.13-venv wget curl
-
-wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass.deb
-sudo apt install -y -f ./hashpass.deb
-rm ./hashpass.deb
--- install/install/install_arm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt -y install python3.13 python3.13-venv wget curl
-
-wget https://gitlab.com/Bunnyton/hashpass/-/raw/main/install/hashpass_arm.deb
-sudo apt install -y -f ./hashpass_arm.deb
-rm ./hashpass_arm.deb
